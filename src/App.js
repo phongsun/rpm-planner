1a43a4fc53d515760f721767020829f0a051f8bb
Pinkergloop <phsun@smu.edu>
2023-01-31 19:35:34 -0700

    basic saving a reminder works

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import { CreateReminder } from './CreateReminder';
 
 function App() {
+  let [createReminder, setCreateReminder] = useState(false);
+  let [reminderList, setReminderList] = useState([]);
+
+  const newReminder = () => {
+    setCreateReminder(true);
+    console.log(createReminder);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>RPM</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <button>Create reminder</button>
+        <button onClick={() => newReminder()}>Create reminder</button>
       </header>
+      <CreateReminder createReminder = {createReminder} 
+      test = "Test"
+      finishReminder = {(reminder, date) => {
+        let newReminder = {reminder:reminder, date:date};
+        //setReminderList([...reminderList, newReminder]);
+        reminderList.push(newReminder);
+        console.log("reminderList length: " + reminderList.length);
+        console.log("reminderList: " + JSON.stringify(reminderList[reminderList.length - 1]))
+        setCreateReminder(false);
+      }}
+      />
     </div>
   );
 }
---
+++ src/CreateReminder.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+export const CreateReminder = (props) => {
+    let thing;
+    let [reminder, createReminder] = useState("");
+    let [date, setDate] = useState(new Date());
+    //alert("Test: " + props.test);
+    if (props.createReminder == true) {
+        thing = <div>
+            <h1>Create new reminder</h1>
+            <input type="text"
+                onChange={(e) => createReminder(e.target.value)}></input>
+            <input
+                type="date"
+                value={props.date}
+                onChange={(e) => setDate(e.target.value)}
+            ></input>
+            <button onClick={() => props.finishReminder(reminder, date)}>OK</button>
+        </div>;
+        //console.log("true");
+    } else {
+        thing = <h1>Ligma balls</h1>;
+        //console.log("false");
+    }
+    return (
+        <div>
+            {thing}
+        </div>
+    );
+}
